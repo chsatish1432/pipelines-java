7731cdecb81b4c6ec00d89bf7e45ed0bc34ecf21
chsatish1432 <56869312+chsatish1432@users.noreply.github.com>
2024-01-15 21:12:23 +0530

    Update install-java-maven.sh

--- install-java-maven.sh
+++ install-java-maven.sh
@@ -14,6 +14,7 @@ else
   #sudo apt-get install -y openjdk-11-jdk
   wget https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz
   tar -xvf openjdk-13.0.1_linux-x64_bin.tar.gz
+  sudo rm -rf /opt/cici-tools/java
   sudo mv jdk-13.0.1 /opt/cici-tools/java
 
   # Update environment variables
@@ -40,6 +41,7 @@ else
   #sudo apt-get install -y maven
   wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
   tar -xvf apache-maven-3.6.3-bin.tar.gz
+  sudo rm -rf /opt/cici-tools/maven
   sudo mv apache-maven-3.6.3 /opt/cici-tools/maven
   
   cat ~/.bashrc|grep MAVEN_HOME
